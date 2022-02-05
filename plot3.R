0dbf1c2193b27e239213bbba2baba3f3d19a54f2
nithyasri7501 <96484611+nithyasri7501@users.noreply.github.com>
2022-02-05 16:41:06 +0530

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,8 @@
+with(new, {
+  plot(Sub_metering_1~datetime, type="l", ylab="Energy sub metering", xlab="")
+  lines(Sub_metering_2~datetime,col='Red')
+  lines(Sub_metering_3~datetime,col='Blue')
+})
+legend("topright", col=c("black","red","blue"), lwd=c(1,1,1), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.copy(png, "plot3.png", width  = 480, height = 480)
+dev.off()
